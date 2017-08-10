56ada5304e9cfe022d249b7bd89badc9a9e605c5
布施和隆 <firebom.sns@gmail.com>
2017-08-10 16:29:22 +0900

    よろしくお願いします。

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // 画面外に出たら破棄する
+        if  (transform.position.z < GameObject.Find("Main Camera").transform.position.z)
+            {
+            Destroy(gameObject);
+        }
+    }
+}
